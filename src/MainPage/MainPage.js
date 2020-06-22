c9caa48529621b9f1faac51dd511eb34ae76b8bc
Vitalii Korol <vitaliikorol@MacBook-Pro.local>
2020-06-22 21:43:24 +0300

    images

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -3,7 +3,52 @@ import './MainPage.scss'
 export const MainPage = () => {
     return (
         <section className="MainPage">
-
+            <ul className="MainPage__list">
+                <li className="MainPage__list_item">
+                    {/*<img src={require('../images/1.png')}/>*/}
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li><li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+                <li className="MainPage__list_item">
+                    <img src="" alt=""/>
+                    <span></span>
+                    <button></button>
+                </li>
+            </ul>
         </section>
     )
 }
