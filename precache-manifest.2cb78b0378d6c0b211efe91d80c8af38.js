3836ba580038dc38ccad0151b552fc207e826e90
Vitalii Korol <vitaliikorol@MacBook-Pro.local>
2020-06-23 11:09:16 +0300

    Updates

---
+++ precache-manifest.2cb78b0378d6c0b211efe91d80c8af38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbaf1d8881fb39694406ed94205018e",
+    "url": "/vitaliikorol.github.io/games/index.html"
+  },
+  {
+    "revision": "32cfb9de6ad8b7cbc702",
+    "url": "/vitaliikorol.github.io/games/static/css/main.5e2bd74d.chunk.css"
+  },
+  {
+    "revision": "0eb0366b630902d84753",
+    "url": "/vitaliikorol.github.io/games/static/js/2.8671d02e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vitaliikorol.github.io/games/static/js/2.8671d02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cfb9de6ad8b7cbc702",
+    "url": "/vitaliikorol.github.io/games/static/js/main.b78f2c99.chunk.js"
+  },
+  {
+    "revision": "fffc94a4863af6711adb",
+    "url": "/vitaliikorol.github.io/games/static/js/runtime-main.fe3b9be5.js"
+  }
+]);
